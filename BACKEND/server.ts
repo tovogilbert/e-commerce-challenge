03a47bfd953dafefb8b99b826d64e67b8ca266e8
tovogilbert <tovogilbert283@gmail.com>
2025-05-13 08:48:23 +0300

    Définir la connection à la base de données

---
+++ BACKEND/server.ts
@@ -0,0 +1,28 @@
+import "module-alias/register";
+import app from "./src/infrastructure/http/app";
+import dotenv from "dotenv";
+import { sequelize } from "./src/infrastructure/db/connection";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3002;
+
+async function startServer() {
+  try {
+    // Connexion à la base
+    await sequelize.authenticate();
+    await sequelize.sync(); // Synchronisation ici
+
+    console.log("Database connected and synchronized");
+
+    // Lancement du serveur HTTP
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to start server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
