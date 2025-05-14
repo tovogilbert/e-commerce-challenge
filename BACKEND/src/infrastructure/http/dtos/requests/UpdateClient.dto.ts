c5f31164d5b4bea024c1af4f2f5e802286e4b383
tovogilbert <tovogilbert283@gmail.com>
2025-05-14 13:50:10 +0300

    Conception des DTOs client

---
+++ BACKEND/src/infrastructure/http/dtos/requests/UpdateClient.dto.ts
@@ -0,0 +1,11 @@
+
+export interface UpdateClientDTO {
+  "email"?: string;
+  "telephone"?: string;
+  "adresse"?: string;
+  "firstName"?: string;
+  "lastName"?: string;
+  "companyName"?: string;
+  "nif"?: string;
+  "stat"?: string;
+}
